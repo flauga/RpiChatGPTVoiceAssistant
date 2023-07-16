409c0b7ba4a614e138fcd9d12eb24a7be9638c52
Shakthi Sairam <80213729+flauga@users.noreply.github.com>
2023-07-16 13:02:07 +0530

    Delete ChatGPTMain.py

--- ChatGPTMain.py
+++
@@ -1,7 +0,0 @@
-import ChatGPTFunctions as CGF
-import speech_recognition as SR
-
-# Use the default microphone as the audio source
-with SR.Microphone() as source:
-    CGF.listen_for_wake_word(source)
-
